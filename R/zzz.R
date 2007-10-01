3b971a2d61702fe2ed83d004c863457063162d7c
Michael Lawrence <lawremi@iastate.edu>
2007-10-01 11:19:52 +0000

    Add utils to deps, Sys.putenv -> Sys.setenv
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/rsbml@27662 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,7 @@
   if(.Platform$OS.type == "windows"){
      if(!exists("Sys.setenv", baseenv())) Sys.setenv <- base::Sys.putenv
      temp <- Sys.getenv("PATH")
-     Sys.putenv(PATH = paste(normalizePath(file.path(libname, pkgname, "libs")), 
+     Sys.setenv(PATH = paste(normalizePath(file.path(libname, pkgname, "libs")), 
                                file.path(Sys.getenv("R_HOME"), "modules", fsep="\\"), temp, sep=";"))
      on.exit(Sys.putenv(PATH = temp))
   }
