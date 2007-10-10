50e3d2b0f3a2185a389d715017ffe8e6f2046807
Michael Lawrence <lawremi@iastate.edu>
2007-10-10 01:42:44 +0000

    Sys.putenv -> Sys.setenv, fix deprecation warning.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/rsbml@27885 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,10 @@
 .First.lib <- function(libname, pkgname)
 {
   if(.Platform$OS.type == "windows"){
-     if(!exists("Sys.setenv", baseenv())) Sys.setenv <- base::Sys.putenv
      temp <- Sys.getenv("PATH")
      Sys.setenv(PATH = paste(normalizePath(file.path(libname, pkgname, "libs")), 
                                file.path(Sys.getenv("R_HOME"), "modules", fsep="\\"), temp, sep=";"))
-     on.exit(Sys.putenv(PATH = temp))
+     on.exit(Sys.setenv(PATH = temp))
   }
   library.dynam("rsbml", pkgname, libname)
 }
