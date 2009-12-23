19d87080cb2584763e57212adced7dd687b91b52
Michael Lawrence <lawremi@iastate.edu>
2009-12-23 12:59:56 +0000

    Add stoichiometryMatrix method on Model for conveniently generating a stoichiometric matrix.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/rsbml@43733 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/Model.R
+++ R/Model.R
@@ -216,3 +216,22 @@ setReplaceMethod("modelHistory", "Model", function(object, value) {
   object
 })
 
+setGeneric("stoichiometryMatrix",
+           function(object, ...) standardGeneric("stoichiometryMatrix"))
+
+setMethod("stoichiometryMatrix", "Model", function(object) {
+  species <- species(object)
+  reactions <- reactions(object)
+  mat <- matrix(0, length(species), length(reactions),
+                dimnames = list(names(species), names(reactions)))
+  setStoich <- function(refList, factor) {
+    col <- rep(seq_along(reactions), sapply(refList, length))
+    refs <- unlist(refList)
+    row <- match(sapply(refs, species), names(species))
+    ind <- row + (col-1)*length(species)
+    mat[ind] <<- mat[ind] + sapply(refs, stoichiometry) * factor
+  }
+  setStoich(lapply(reactions, reactants), -1)
+  setStoich(lapply(reactions, products), 1)
+  mat
+})
